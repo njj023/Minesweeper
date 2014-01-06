5bbfc55b487979de67a7fecd0674a4cfe2a3a23d
Nitin Joshi <njoshi023@gmail.com>
2014-01-06 00:31:26 -0500

    Fix proper randomizing of mines, display all mines when lost, declare win when user clicks on all non mines

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-var msApp = angular.module('msApp', ['ngRoute']); // Initialize the AngularJS system
+// Initialize the AngularJS system
+var msApp = angular.module('msApp', ['ngRoute']);
+
+// Add right click directive
+msApp.directive('ngRightClick', function($parse) {
+    return function(scope, element, attrs) {
+        var fn = $parse(attrs.ngRightClick);
+        element.bind('contextmenu', function(event) {
+            scope.$apply(function() {
+                event.preventDefault();
+                fn(scope, {$event:event});
+            });
+        });
+    };
+});
--- js/controllers/game.js
+++ js/controllers/game.js
@@ -1,18 +1,25 @@
-msApp.controller('GameCtrl', function($scope, Game) {
-  	$scope.rowLength = Game.rows;
-  	$scope.colLength = Game.cols;
-  	$scope.numMines = Game.numMines;
+msApp.controller('GameCtrl', function($scope, $location, Game) {
+    if (!Game.rows || !Game.cols || !Game.numMines) {
+        $location.path('/start');
+    }
 
-    $scope.setGrid = function() {
-		$scope.grid = new Array();
+    $scope.startGame = function() {
+		$scope.rowLength = parseInt(Game.rows);
+        $scope.colLength = parseInt(Game.cols);
+        $scope.numMines = parseInt(Game.numMines);
+        $scope.numClicked = 0;
+        $scope.grid = new Array();
+        $scope.won = false;
+        $scope.lost = false;
+        $scope.showMines = false;
 
 		for (var i = 0; i < $scope.rowLength; i++) {
 			$scope.grid[i] = new Array($scope.colLength);
 		}
 
 		$scope.initializeGrid();
-
 		$scope.setMines();
+        $scope.shuffleMines();
     };
 
     $scope.initializeGrid =  function() {
@@ -21,78 +28,131 @@ msApp.controller('GameCtrl', function($scope, Game) {
     		for (var j = 0; j < $scope.colLength; j++) {
     			$scope.grid[i][j] = {
     				clicked: false,
-    				val: null,
-    				adjNum: null
+    				containsMine: false,
+    				adjNum: null,
+                    flagged: false
     			};
     		}
     	}
+
+        $scope.totalBoxes = $scope.rowLength * $scope.colLength;
     };
     
     $scope.setMines = function() {
-    	var setMines = [];
-    	for (var i = 0; i < $scope.numMines; i++ ) {
-    		
-    		var randRow = Math.floor( (Math.random() * $scope.rowLength) );
-    		var randCol = Math.floor( (Math.random() * $scope.colLength) );
+    	var setMines = [],
+            totalMinesSet = 0;
 
-    		if (!$scope.grid[randRow][randCol].val !== 'x')
-    			$scope.grid[randRow][randCol].val = 'x';
-    	}
+        while (totalMinesSet < $scope.numMines) {
+            var row = Math.floor(totalMinesSet / $scope.colLength);
+            var col = totalMinesSet % $scope.colLength;
+            $scope.grid[row][col].containsMine = true;
+            totalMinesSet++;
+        }
+    };
+
+    $scope.shuffleMines = function() {
+        var shuffleCount = 0;
+        while (shuffleCount < $scope.numMines) {
+            var row = Math.floor(shuffleCount / $scope.colLength);
+            var col = shuffleCount % $scope.colLength;
+
+            var randRow = Math.floor( (Math.random() * $scope.rowLength) );
+            var randCol = Math.floor( (Math.random() * $scope.colLength) );
+
+            // Swap current mine with a random location
+            var temp = $scope.grid[row][col];
+            $scope.grid[row][col] = $scope.grid[randRow][randCol];
+            $scope.grid[randRow][randCol] = temp;
+
+            shuffleCount++;
+        }
     };
 
     $scope.containsMine = function(row, col) {
-    	return $scope.grid[row][col].val === 'x';
+    	return $scope.grid[row][col].containsMine;
     };
 
-    $scope.renderMines = function(row, col) {
-		if (!$scope.grid[row][col].clicked) {
-			if ($scope.grid[row][col].val === 'x') {
+    $scope.checkForMines = function(row, col) {
+        // Check if row or column invalid
+        if ($scope.grid[row] === undefined || $scope.grid[row][col] === undefined) {
+            return;
+        }
+
+        if (!$scope.grid[row][col].clicked) {
+            $scope.grid[row][col].clicked = true;
+            $scope.numClicked++;
+			if ($scope.grid[row][col].containsMine) {
 				$scope.lost = true;
+                $scope.showMines = true;
 				return;
 			}
 
 			$scope.grid[row][col].adjNum = $scope.calcAdjNum(row, col);
+            
+            // Recurse on all adjacent mines
+            if ($scope.grid[row][col].adjNum == 0) {
+                $scope.checkForMines(row - 1, col - 1);
+                $scope.checkForMines(row - 1, col);
+                $scope.checkForMines(row - 1, col + 1);
+                $scope.checkForMines(row, col - 1);
+                $scope.checkForMines(row, col + 1);
+                $scope.checkForMines(row + 1, col - 1);
+                $scope.checkForMines(row + 1, col);
+                $scope.checkForMines(row + 1, col + 1);
+            }
+
 			$scope.won = $scope.checkWin();
 		}
     };
 
-    $scope.calcAdjNum = function(row, col) {
-    	var counter = 0,
-    		grid = $scope.grid;
+    $scope.calcAdjNum = function(row, col) {    	
+        if ($scope.grid[row] === undefined || $scope.grid[row][col] === undefined) {
+            return null;
+        }
+        var counter = 0,
+    		grid = $scope.grid,
+            isMine = $scope.isMine;
 
 		// check top row
-		if (row != 0) {
-			if (grid[row - 1][col - 1].val === "x")
-				counter++;
-			if (grid[row - 1][col].val === "x")
-				counter++;
-			if (grid[row - 1][col + 1].val === "x")
-				counter++;
-		}
+        counter += isMine(row - 1, col - 1 ) + isMine(row - 1, col) + isMine(row - 1, col + 1);
 
-		// check current row
-		if (grid[row][col - 1].val === "x")
-			counter++;
-		if (grid[row][col + 1].val === "x")
-			counter++;
-			
+        // check current row
+        counter += isMine(row, col - 1 ) + isMine(row, col + 1);
 
-		// check bottom row
-		if (row != $scope.rowLength - 1) {
-			if (grid[row + 1][col - 1].val === "x")
-				counter++;
-			if (grid[row + 1][col].val === "x")
-				counter++;
-			if (grid[row + 1][col + 1].val === "x")
-				counter++;
-		}
+        // check bottom row
+        counter += isMine(row + 1, col - 1 ) + isMine(row + 1, col) + isMine(row + 1, col + 1);
 
     	return counter;
     }
 
+    $scope.isMine = function(row, col) {
+        var isMine = $scope.grid[row] !== undefined && $scope.grid[row][col] != undefined && $scope.grid[row][col].containsMine;
+        return isMine ? 1 : 0;
+    }
+
+    // Game determined as won if user has opened all boxes minus the mine ones
     $scope.checkWin = function() {
-    	return false;
+        console.log($scope.numClicked, $scope.numMines, $scope.totalBoxes);
+    	return $scope.numMines + $scope.numClicked == $scope.totalBoxes
     };
 
-    $scope.setGrid();
+    $scope.changeState = function(row, col) {
+        var currState = grid[row][col].userState;
+        var newState;
+        switch(currState) {
+            case "!":
+                newState = "?";
+                break;
+            case "?":
+                newState = "";
+                break;
+            default:
+                newState = "!";
+        }
+
+        grid[row][col].userState = newState;
+
+    }
+
+    $scope.startGame();
 });
